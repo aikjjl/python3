2b2db9be011a735e1dfc62970f5a39c8625b45d8
aikjjl <45061188+aikjjl@users.noreply.github.com>
2020-07-13 14:43:01 +0800

    1

--- python3/copy文件.py
+++ python3/copy文件.py
@@ -1,27 +1,31 @@
+
+#多进程复制文件
+
+
 import os
 from multiprocessing import Pool
 
-def copyFileTask(name, oldFolderName, newFolderName):
+def copyFileTask(name, oldFName, newFName):
     #完成copy文件的功能
-    fr = open(oldFolderName+"/"+name)
-    fw = open(newFolderName+"/"+name, "w")
+    fr = open(oldFName+"/"+name)
+    fw = open(newFName+"/"+name, "w")
     content = fr.read()
     fw.write(content)
     fr.close()
     fw.close()
 
 def main():
-    oldFloderName = input("请输入要copy的文件夹：")
+    oldFName = input("请输入要copy的文件夹：")
     #创建文件夹
-    newFolderName = oldFolderName +"-复件"
-    os.mkdir(newFolderName)
+    newFName = oldFName +"-复件"
+    os.mkdir(newFName)
 
     #获取old文件夹中所有文件的名称
-    fileNames = os.listdir(oldFloderName)
+    FNames = os.listdir(oldFName)
 
     #多进程方式copy文件到new文件夹
     pool = Pool(5)
-    pool.apply_async(copyFileTask, args=(name, oldFolderName, newFolderName))
+    pool.apply_async(copyFileTask, args=(name, oldFName, newFName))
 
     pool.close()
     pool.join()
---
+++ 生成器，闭包，装饰器/生成器.sh
@@ -0,0 +1,14 @@
+
+#生成器
+#函数中出现yield，不能再当成普通函数，而是生成器
+
+def CreateNum():
+	print("start")
+	a, b = 0, 1
+	for i in range(5):
+		print("1")
+		yield b
+		a, b = b, a+b
+		print("2")
+	print("stop")
+
