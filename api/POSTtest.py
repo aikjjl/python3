15440a27a721aa26691570c4f7f70ea0e8aaada3
v_junjinglu <45061188+aikjjl@users.noreply.github.com>
2022-05-08 23:24:42 +0800

    Update POSTtest.py
    1

--- api/POSTtest.py
+++ api/POSTtest.py
@@ -7,24 +7,23 @@ import requests
 post请求参数一部分在url里面，另一部分在body里
 
 1，第一种application / json和dict转换, 传json参数
-{"i":"xxx","i2":"xxxx","i3":"true"}
+{"i": "xxx", "i2": "xxxx", "i3": "true"}
 
-2,第二种 application/x-www-form-urlencodeed，传data参数
-i=xxx&i2=xxx&i3=fales
+2, 第二种
+application / x - www - form - urlencodeed，传data参数
+i = xxx & i2 = xxx & i3 = fales
 
-3,第三种 multipart、form-data：这是一种表单格式的
+3, 第三种
+multipart、form - data：这是一种表单格式的
 文件上传，图片上传等混合式
 
-4，第四种 text/xml
-
-
+4，第四种
+text / xml
 
 post传json参数
 1，传json参数，自动转json
-d={"a":1, "b":"c"}
+d = {"a": 1, "b": "c"}
 requests.post(url=url, json=d)
 
-2,传data参数
-requests.post(url=url,data=json.dumps(d))
-
-
+2, 传data参数
+requests.post(url=url, data=json.dumps(d))
